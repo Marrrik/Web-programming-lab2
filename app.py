1bb7c479c7c9244ba5397c11318135b041a4edbe
Марк Старченко <slaidsuper@gmail.com>
2023-09-06 17:26:13 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>Старченко Марк, Лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+            <h1>ФБ, НГТУ, Лабораторная 1</h1>
+        </header>
+
+        <h1>web-сервер на flask</h1>
+
+        <footer>
+            ©<i>Марк Старченко, ФБИ-14, 2 курс, 2023</i>
+        </footer>
+    </body>
+</html>
+"""

8381fb7ff3db1274c8fd61e42f50dcda5d61b555
Марк Старченко <slaidsuper@gmail.com>
2023-10-06 12:54:28 +0700

    Рефакторинг: выделение второй лабораторноi

--- app.py
+++ app.py
@@ -1,42 +1,8 @@
 from flask import Flask, redirect, url_for, render_template
 from lab import lab
+from lab2 import laba2
+
 
 app = Flask(__name__)
 app.register_blueprint(lab)
-
-@app.route('/lab2/example')
-def example():
-    name = 'Мрак Старченко'
-    lab = 'Лабораторная работа 2'
-    num_group = 'ФБИ-14'
-    rang = '2 курс'
-    fruits = [
-        {'name': 'яблоки', 'price':100},
-        {'name': 'грущи', 'price':120},
-        {'name': 'апельсины', 'price':80},
-        {'name': 'мандарины', 'price':95},
-        {'name': 'манго', 'price':321}
-    ]
-
-    books = [
-        {"Author": "George Orwell", "Title": "1984", "Genre": "Science Fiction", "Number_of_Pages": 328},
-        {"Author": "Fyodor Dostoevsky", "Title": "Crime and Punishment", "Genre": "Classic Literature", "Number_of_Pages": 672},
-        {"Author": "J.K. Rowling", "Title": "Harry Potter and the Philosopher's Stone", "Genre": "Fantasy", "Number_of_Pages": 256},
-        {"Author": "J.R.R. Tolkien", "Title": "The Lord of the Rings", "Genre": "Fantasy", "Number_of_Pages": 1216},
-        {"Author": "Agatha Christie", "Title": "And Then There Were None", "Genre": "Detective", "Number_of_Pages": 256},
-        {"Author": "Leo Tolstoy", "Title": "War and Peace", "Genre": "Classic Literature", "Number_of_Pages": 1225},
-        {"Author": "George R.R. Martin", "Title": "A Game of Thrones", "Genre": "Fantasy", "Number_of_Pages": 694},
-        {"Author": "Jane Austen", "Title": "Pride and Prejudice", "Genre": "Novel", "Number_of_Pages": 432},
-        {"Author": "Ray Bradbury", "Title": "Fahrenheit 451", "Genre": "Science Fiction", "Number_of_Pages": 249},
-        {"Author": "George Orwell", "Title": "Animal Farm", "Genre": "Satire", "Number_of_Pages": 144}
-    ]
-
-    return render_template('example.html', name=name, lab=lab, num_group=num_group, rang=rang, fruits = fruits, books=books)
-
-@app.route('/lab2/')
-def lab2():
-    return render_template('lab2.html')
-
-@app.route('/lab2/mem/')
-def mem():
-    return render_template('mem.html')
+app.register_blueprint(laba2)

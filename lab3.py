3731b2a159c3ef3c6312aa16ef5d463d16731624
Марк Старченко <slaidsuper@gmail.com>
2023-10-07 00:27:52 +0700

    Жд билеты и предыдущее задание

--- lab3.py
+++ lab3.py
@@ -17,3 +17,52 @@ def lab3_form1():
         errors['age'] = 'Заполните поле!'
     sex = request.args.get('sex')
     return render_template("form1.html", user=user, age=age, sex=sex, errors=errors)
+
+
+@laba3.route('/lab3/orders')
+def lab3_order():
+    return render_template("orders.html")
+
+
+@laba3.route('/lab3/pay')
+def lab3_pay():
+    price = 0
+    drink = request.args.get('drink')
+    if drink == 'cofee':
+        price = 120
+    elif drink == 'black-tea':
+        price = 80
+    elif drink == 'green-tea':
+        price = 70
+    else:
+        price = ''
+    if request.args.get('milk') == 'on':
+        price +=30
+    if request.args.get('sugar') == 'on':
+        price +=10
+
+    return render_template("pay.html", price=price)
+
+
+@laba3.route('/lab3/itog')
+def lab3_itog():
+    return render_template("itog.html")
+
+
+@laba3.route('/lab3/gd')
+def lab3_gd():
+    full_name = request.args.get('full_name')
+    age = request.args.get('age')
+    ticket_type = request.args.get('ticket_type')
+    bunk = request.args.get('bunk')
+    luggage = request.args.get('luggage')
+    departure_point = request.args.get('departure_point')
+    destination = request.args.get('destination')
+    travel_date = request.args.get('travel_date')
+    return render_template("tiket.html", full_name=full_name, age=age, ticket_type=ticket_type, bunk=bunk,luggage=luggage,departure_point=departure_point,
+                           destination=destination,travel_date=travel_date)
+
+
+# @laba3.route('/lab3/buy_ticket')
+# def lab3_buy_ticket():
+#     return render_template('buy_ticket.html')

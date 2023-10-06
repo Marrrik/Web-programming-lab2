545990289e0d22ca4d8011201968b29deb6fcb0c
Марк Старченко <slaidsuper@gmail.com>
2023-10-06 22:15:21 +0700

    Валидация

--- lab3.py
+++ lab3.py
@@ -8,7 +8,12 @@ def lab3():
 
 @laba3.route('/lab3/form1')
 def lab3_form1():
+    errors = {}
     user = request.args.get('user')
+    if user == '':
+        errors['user'] = 'Заполните поле!'
     age = request.args.get('age')
+    if age == '':
+        errors['age'] = 'Заполните поле!'
     sex = request.args.get('sex')
-    return render_template("form1.html", user=user, age=age, sex=sex)
+    return render_template("form1.html", user=user, age=age, sex=sex, errors=errors)

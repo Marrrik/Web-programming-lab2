3af56c57c92eefe448eeffc33b3276f44519c1b6
Марк Старченко <slaidsuper@gmail.com>
2023-10-18 12:48:07 +0700

    Логин пароль

--- lab4.py
+++ lab4.py
@@ -4,3 +4,25 @@ laba4 = Blueprint('laba4', __name__)
 @laba4.route('/lab4/')
 def lab4():
     return render_template("lab4.html")
+
+
+@laba4.route('/lab4/login', methods=['GET', 'POST'])
+def lab4_login():
+    if request.method == 'GET':
+        return render_template("login.html")
+
+    username = request.form.get('username')
+    password = request.form.get('password')
+    error = None 
+    if not username and not password:
+        error = 'Нет логина и пароля'
+    elif not username:
+        error = 'Нет логина'
+    elif not password:
+        error = 'Нет пароля'
+    elif username == 'Mark_St' and password == '2003':
+        return render_template("success.html", username=username)
+    else:
+        error = 'Неверный логин или пароль!'
+        
+    return render_template("login.html", error=error, username=username, password=password)

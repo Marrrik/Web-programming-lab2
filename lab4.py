c8062bca68bd291191b47d8c775a01a1c38d0acd
Марк Старченко <slaidsuper@gmail.com>
2023-10-18 13:48:16 +0700

    Холодильник

--- lab4.py
+++ lab4.py
@@ -24,5 +24,27 @@ def lab4_login():
         return render_template("success.html", username=username)
     else:
         error = 'Неверный логин или пароль!'
-        
+
     return render_template("login.html", error=error, username=username, password=password)
+
+
+@laba4.route('/lab4/freeze', methods=['GET', 'POST'])
+def lab4_freeze():
+    if request.method == 'GET':
+        return render_template("freeze.html")
+    
+    temperature = request.form.get('temperature')
+    if  not temperature:
+        result = 'ошибка: не задана температура!'
+    elif int(temperature) < -12:
+        result = 'не удалось установить температуру — слишком низкое значение'
+    elif int(temperature) > -1:
+        result = 'не удалось установить температуру — слишком высокое значение'
+    elif int(temperature) > -12 and int(temperature) < -9:
+        result = f"Установлена температура: {temperature}°C ***"
+    elif int(temperature) >= -8 and int(temperature) <= -5:
+        result = f"Установлена температура: {temperature}°C **"
+    elif int(temperature) >= -4 and int(temperature) <= -1:
+        result = f"Установлена температура: {temperature}°C *"
+
+    return render_template("freeze.html", result=result)

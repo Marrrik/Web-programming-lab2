2b7322ad69fd13c21533c323ab469791f563b8c5
Марк Старченко <slaidsuper@gmail.com>
2023-10-23 01:16:13 +0700

    Всё остальное

--- lab4.py
+++ lab4.py
@@ -48,3 +48,79 @@ def lab4_freeze():
         result = f"Установлена температура: {temperature}°C *"
 
     return render_template("freeze.html", result=result)
+
+
+@laba4.route('/lab4/zerno', methods=['GET', 'POST'])
+def lab4_zerno():
+    if request.method == 'GET':
+        return render_template("zerno.html")
+    grain = request.form.get('grain')
+    weight = float(request.form.get('weight'))
+    price = 0
+    skidka = None
+
+    if grain == 'Ячмень':
+        price = 12000
+    elif grain == 'Овёс':
+        price = 8500
+    elif grain == 'Пшеница':
+        price = 8700
+    elif grain == 'Рожь':
+        price = 14000
+    else:
+        price = 0
+    
+    summ = price * weight
+    if weight >= 50 and weight <= 500:
+        summ = (price * weight) * 0.9
+        skidka_itog = (price * weight) * 0.1
+        skidka = f'Скидка 10% за объём более 50 тон: = {skidka_itog}'
+    elif weight < 50:   
+        skidka = 'Скидка 10% за объём не присвена'
+    else:
+        skidka = None
+
+ 
+    if weight > 500:
+        errors = 'У нас нет столько зерна'
+        return render_template("zerno.html", errors=errors)
+    elif weight < 0:
+        errors = 'Не верно указан объём закупки'
+        return render_template("zerno.html", errors=errors)
+    
+    return render_template("zerno.html", grain=grain, weight=weight, summ=summ, skidka=skidka)
+
+
+@laba4.route('/lab4/cookies', methods=['GET', 'POST'])
+def lab4_cookies():
+    if request.method == 'GET':
+        return render_template("cookies.html")
+
+    textColor = request.form.get('textColor')
+    backgroundСolor = request.form.get('backgroundСolor')
+    # print()
+    fontSize = int(request.form.get('fontSize'))
+    errorss = None
+
+    if not fontSize:
+        errorss = 'введите размер!!!'
+        return redirect(url_for('lab4_cookies', errorss=errorss))
+    if textColor == backgroundСolor:
+        errorss = 'Цвет текста не должен совпадать с цветом фона.'
+        return redirect(url_for('lab4_cookies', errorss=errorss))
+
+    
+    if fontSize>30 or fontSize<5:
+        errorss = 'Размер текста должен быть от 5px до 30px.'
+        return redirect(url_for('lab4_cookies', errorss=errorss))
+
+    headers = {
+        'Set-Cookie': [
+            'textColor=' + textColor + 'path=/',
+            'backgroundСolor=' + backgroundСolor + 'path=/',
+            'fontSize=' + str(fontSize) + 'path=/'
+        ],
+        'Location': '/lab4/cookies'
+    }
+
+    return '', 303, headers
